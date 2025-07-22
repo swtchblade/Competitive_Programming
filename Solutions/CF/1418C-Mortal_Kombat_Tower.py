49ec7dcaf34863feb7727b942607f7a70c74df50
Nathan <81914778+swtchblade@users.noreply.github.com>
2025-07-21 21:17:44 -0400

    Create 1418C-Mortal_Kombat_Tower.py
    https://codeforces.com/contest/1418/problem/C

---
+++ Solutions/CF/1418C-Mortal_Kombat_Tower.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    d = [[10**6]*(n+1) for _ in range(2)]
+    d[1][0]=0
+    for i in range(0,n):
+        d[0][i+1] = min(d[0][i+1],d[1][i]+a[i])
+        d[1][i+1] = min(d[1][i+1], d[0][i])
+        if i<n-1:
+            d[0][i+2] = min(d[0][i+2], d[1][i]+a[i]+a[i+1])
+            d[1][i+2] = min(d[1][i+2], d[0][i])
+    print(min(d[0][n], d[1][n]))
