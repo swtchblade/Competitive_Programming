9860f2292317aefaa71b1e247f4d17f61b3dfd26
Nathan <81914778+swtchblade@users.noreply.github.com>
2025-07-21 20:54:34 -0400

    Create template.cpp
    Benq-inspired C++ template
    
    Mostly tried to add/remove things based on their usefulness at gold/cf d2 level

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define tcT template<class T
+tcT> using V=vector<T>; 
+using ll=long long;using ld=long double;using db=double;using str=string;
+using pi=pair<int,int>;using pl=pair<ll,ll>;
+using vi=V<int>;using vl=V<ll>;using vb=V<bool>;using vs=V<str>;using vpi=V<pi>;
+#define len(a) a.size()
+#define rz resize
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define FORJ(i,a,b,c) for(int i=a;i<b;i+=c)
+#define ROF(i,b,a) for(int i=b;i>a;i--)
+#define ROFJ(i,b,a,c) for(int i=b;i>a;i-=c)
+#define each(a,x) for (auto& a:x)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+#define so(x) sort(all(x))
+#define sor(x) sort(rall(x))
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define ft front()
+#define bk back()
+#define mp make_pair
+#define f first
+#define s second
+int main() { 
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+}
