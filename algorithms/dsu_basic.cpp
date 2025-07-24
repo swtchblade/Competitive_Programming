8c5d3f6f027dbea4fcab1936439d5546daa3aa92
Nathan <81914778+swtchblade@users.noreply.github.com>
2025-07-24 16:17:32 -0400

    Update and rename dsu_basic to dsu_basic.cpp

--- algorithms/dsu_basic.cpp
+++ algorithms/dsu_basic.cpp
@@ -1,5 +1,5 @@
 struct DSU {
-	vi e;void init(int N){e=vi(N,-1);}
+	V<int> e;void init(int N){e=V<int>(N,-1);}
 	int get(int x){return e[x] < 0?x:e[x]=get(e[x]);} 
 	bool same(int a,int b){return get(a)==get(b);}
 	int size(int x){return -e[get(x)];}
