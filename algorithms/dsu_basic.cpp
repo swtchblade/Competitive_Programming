b105e8f96f3a8a6ea941ed6eb7db4eab2777112d
Nathan <81914778+swtchblade@users.noreply.github.com>
2025-07-26 21:35:54 -0400

    Update dsu_basic.cpp

--- algorithms/dsu_basic.cpp
+++ algorithms/dsu_basic.cpp
@@ -1,9 +1,11 @@
-struct DSU {
-	V<int> e;void init(int N){e=V<int>(N,-1);}
-	int get(int x){return e[x] < 0?x:e[x]=get(e[x]);} 
+template<int n> struct DSU {
+	vi e;int c;
+	DSU():e(n, -1),c(n){}
+	int get(int x){int r=x;while(e[r]>=0){r=e[r];}
+    		while (x!=r){int p=e[x];e[x]=r;x=p;}return r;}
 	bool same(int a,int b){return get(a)==get(b);}
-	int size(int x){return -e[get(x)];}
+	int sz(int x){return -e[get(x)];}
 	bool uni(int x, int y){
-		x = get(x), y = get(y);if (x == y){return 0;}
-		if (e[x] > e[y]){swap(x,y);}
-		e[x]+=e[y];e[y]=x;return 1;}};
+		x=get(x),y=get(y);if (x==y){return 0;}
+		if (e[x]>e[y]){swap(x,y);}
+		e[x]+=e[y];e[y]=x;c--;return 1;}};
