bf99d67f4e430cf57dccdba1a7c0f01dea7c90b3
Nathan <81914778+swtchblade@users.noreply.github.com>
2025-10-28 21:55:22 -0400

    Add minimal C++ template structure
    Initial implementation of a minimal C++ template structure.

---
+++ algorithms/template-minimal.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define tcT template<class T
+tcT> using V=vector<T>;
+using ll=long long;using str=string;
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define ROF(i,b,a) for(int i=b;i>a;i--)
+#define each(a,x) for (auto& a:x)
+#define all(x) begin(x),end(x)
+#define so(x) sort(all(x))
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+int main() { 
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+}
