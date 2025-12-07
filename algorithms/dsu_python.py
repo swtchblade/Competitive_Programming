8cbeafd5d8981bc953f72d066423617d5728f832
Nathan <81914778+swtchblade@users.noreply.github.com>
2025-12-07 10:24:41 -0500

    Update dsu_python.py

--- algorithms/dsu_python.py
+++ algorithms/dsu_python.py
@@ -1,17 +1,13 @@
 class DSU:
-	def __init__(self,n):
-		self.p = list(range(n))
-		self.s = [1]*n
-	def find(self,x):
+	def __init__(self,n): self.p = list(range(n)); self.s = [1]*n
+	def get(self,x):
 		if self.p[x] == x:return x
-		self.p[x] = self.find(self.p[x])
+		self.p[x] = self.get(self.p[x])
 		return self.p[x]
-	def unite(self,x,y):
-		xr = self.find(x)
-		yr = self.find(y)
+	def uni(self,x,y):
+		xr, yr = self.get(x),self.get(y)
 		if xr == yr:return False
 		if self.s[xr] < self.s[yr]:xr, yr = yr, xr
-		self.p[yr] = xr
-		self.s[xr] += self.s[yr]
+		self.p[yr] = xr; self.s[xr] += self.s[yr]
 		return True
-	def connected(self,x,y):return self.find(x) == self.find(y)
+	def same(self,x,y):return self.get(x) == self.get(y)
